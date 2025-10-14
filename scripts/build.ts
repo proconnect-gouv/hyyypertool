68cbf963b1dae290873b9aca38d30c9fdd354062
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-10-14 14:57:23 +0200

    🔥 Delete build bloc "lit"

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,32 +4,6 @@ const minify = true;
 
 {
   const { logs, outputs, success } = await Bun.build({
-    entrypoints: ["lit"],
-    outdir: "./public/built",
-    minify,
-  });
-  console.log({ logs, outputs, success });
-}
-{
-  const lit_files = [
-    "async-directive.js",
-    "decorators.js",
-    "directive.js",
-    "directives/repeat.js",
-    "html.js",
-  ];
-  const { logs, outputs, success } = await Bun.build({
-    entrypoints: lit_files.map((entrypoint) =>
-      Bun.resolveSync(`lit/${entrypoint}`, process.cwd()),
-    ),
-    outdir: `./public/built/node_modules/lit`,
-    minify,
-  });
-  console.log({ logs, outputs, success });
-}
-
-{
-  const { logs, outputs, success } = await Bun.build({
     entrypoints: [
       "./sources/app/layout/src/_client/nprogress.ts",
       "./sources/welcome/api/src/_client/hyyypertitle.ts",
