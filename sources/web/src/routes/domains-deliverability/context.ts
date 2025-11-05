a40b7844e38ef4a6d241e41d0f4e00b1957b644e
rebeccadumazert <rebeccadumazert@hotmail.fr>
2025-11-05 16:50:46 +0100

    ✨ Update context

--- sources/web/src/routes/domains-deliverability/context.ts
+++ sources/web/src/routes/domains-deliverability/context.ts
@@ -1,12 +1,27 @@
 //
 
 import type { App_Context } from "#src/middleware/context";
-import { z } from "zod";
+import type { IdentiteProconnect_PgDatabase } from "@~/identite-proconnect.database";
+import type { Env } from "hono";
+import { useRequestContext } from "hono/jsx-renderer";
 
 //
 
-export type ContextType = App_Context;
+export async function loadEmailDeliverabilityPageVariables(
+  pg: IdentiteProconnect_PgDatabase,
+) {
+  const whitelist = await pg.query.email_deliverability_whitelist.findMany({
+    orderBy: (table, { desc }) => [desc(table.verified_at)],
+  });
+
+  return { whitelist };
+}
+
+export interface ContextVariablesType extends Env {
+  Variables: Awaited<ReturnType<typeof loadEmailDeliverabilityPageVariables>>;
+}
+export type ContextType = App_Context & ContextVariablesType;
 
 //
 
-export const PageQuery_Schema = z.object({});
+export const usePageRequestContext = useRequestContext<ContextType>;
