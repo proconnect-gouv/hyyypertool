63ae8a0e1285a60001a8ab2c906ec4af8738179b
Douglas Duteil <douglasduteil@gmail.com>
2025-11-26 14:25:10 +0100

    ♻️ refactor(crisp): migrate to singleton dependency injector pattern (#1224)

--- sources/web/src/routes/moderations/:id/rejected/index.ts
+++ sources/web/src/routes/moderations/:id/rejected/index.ts
@@ -1,13 +1,11 @@
 //
 
 import type { HtmxHeader } from "#src/htmx";
-import { CrispApi } from "#src/lib/crisp";
 import {
   MODERATION_EVENTS,
   reject_form_schema,
   type RejectedModeration_Context,
 } from "#src/lib/moderations";
-import { set_crisp_config } from "#src/middleware/crisp";
 import { GetModerationWithUser } from "#src/queries/moderations";
 import { zValidator } from "@hono/zod-validator";
 import { EntitySchema } from "@~/core/schema";
@@ -19,20 +17,18 @@ import { mark_as_rejected } from "./mark_as_rejected.workflow";
 
 export default new Hono<ContextType>().patch(
   "/",
-  set_crisp_config(),
   zValidator("param", EntitySchema),
   zValidator("form", reject_form_schema),
   async function PATCH({
     text,
     req,
-    var: { identite_pg, userinfo, crisp_config, config },
+    var: { identite_pg, userinfo, crisp, config },
   }) {
     const { id: moderation_id } = req.valid("param");
     const { message, reason, subject } = req.valid("form");
 
     const get_moderation_with_user = GetModerationWithUser(identite_pg);
     const moderation = await get_moderation_with_user(moderation_id);
-    const crisp = CrispApi(crisp_config);
     const context: RejectedModeration_Context = {
       crisp,
       moderation,
