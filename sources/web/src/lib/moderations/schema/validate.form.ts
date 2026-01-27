5995c21bb54807053035bcaa64a7480d9e7b2f19
Douglas DUTEIL <douglasduteil@gmail.com>
2026-01-27 21:30:18 +0100

    add_domain should stay optional

--- sources/web/src/lib/moderations/schema/validate.form.ts
+++ sources/web/src/lib/moderations/schema/validate.form.ts
@@ -6,9 +6,9 @@ import { z } from "zod";
 //
 
 export const validate_form_schema = z.object({
-  add_domain: z.stringbool(),
+  add_domain: z.stringbool().default(false),
   add_member: z.enum(["AS_INTERNAL", "AS_EXTERNAL"]),
-  send_notification: z.stringbool(),
+  send_notification: z.stringbool().default(false),
   verification_type: UserOrganizationLinkVerificationTypeSchema.or(
     z.literal("null").transform(() => null),
   ).default(null),
