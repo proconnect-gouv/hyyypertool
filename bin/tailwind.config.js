1f0df6ec8319e709846c21801e0200f642fabff8
Douglas DUTEIL <douglasduteil@gmail.com>
2026-02-05 16:01:22 +0100

    🔧 update tailwind config

--- bin/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["../sources/**/*.tsx", "../sources/**/*.ts"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/typography")],
-};
